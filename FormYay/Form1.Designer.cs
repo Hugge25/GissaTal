366120f5c823acd1f42596650e0bae2f661d8028
Hugge25 <111962456+Hugge25@users.noreply.github.com>
2023-06-02 10:52:49 +0200

    Add files via upload

--- FormYay/Form1.Designer.cs
+++ FormYay/Form1.Designer.cs
@@ -29,7 +29,7 @@
         private void InitializeComponent()
         {
             this.label1 = new System.Windows.Forms.Label();
-            this.StörstTal = new System.Windows.Forms.ListBox();
+            this.lbxStörstaTalet = new System.Windows.Forms.ListBox();
             this.tbxMinaResultat = new System.Windows.Forms.TextBox();
             this.btnStartaSpelet = new System.Windows.Forms.Button();
             this.btnSpelaIgen = new System.Windows.Forms.Button();
@@ -40,34 +40,38 @@
             this.tbxGissa = new System.Windows.Forms.TextBox();
             this.lblDatornsTal = new System.Windows.Forms.Label();
             this.lblResultat = new System.Windows.Forms.Label();
+            this.gbxSpelet = new System.Windows.Forms.GroupBox();
+            this.gbxSpeldata = new System.Windows.Forms.GroupBox();
+            this.gbxSpelet.SuspendLayout();
+            this.gbxSpeldata.SuspendLayout();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AccessibleName = "";
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(47, 122);
+            this.label1.Location = new System.Drawing.Point(16, 41);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(72, 15);
             this.label1.TabIndex = 0;
             this.label1.Text = "Största talet:";
             // 
-            // StörstTal
+            // lbxStörstaTalet
             // 
-            this.StörstTal.FormattingEnabled = true;
-            this.StörstTal.ItemHeight = 15;
-            this.StörstTal.Items.AddRange(new object[] {
+            this.lbxStörstaTalet.FormattingEnabled = true;
+            this.lbxStörstaTalet.ItemHeight = 15;
+            this.lbxStörstaTalet.Items.AddRange(new object[] {
             "10",
             "20"});
-            this.StörstTal.Location = new System.Drawing.Point(125, 113);
-            this.StörstTal.Name = "StörstTal";
-            this.StörstTal.Size = new System.Drawing.Size(90, 34);
-            this.StörstTal.TabIndex = 1;
+            this.lbxStörstaTalet.Location = new System.Drawing.Point(94, 32);
+            this.lbxStörstaTalet.Name = "lbxStörstaTalet";
+            this.lbxStörstaTalet.Size = new System.Drawing.Size(90, 34);
+            this.lbxStörstaTalet.TabIndex = 1;
             // 
             // tbxMinaResultat
             // 
             this.tbxMinaResultat.Enabled = false;
-            this.tbxMinaResultat.Location = new System.Drawing.Point(383, 257);
+            this.tbxMinaResultat.Location = new System.Drawing.Point(338, 177);
             this.tbxMinaResultat.Multiline = true;
             this.tbxMinaResultat.Name = "tbxMinaResultat";
             this.tbxMinaResultat.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
@@ -77,17 +81,18 @@
             // 
             // btnStartaSpelet
             // 
-            this.btnStartaSpelet.Location = new System.Drawing.Point(408, 113);
+            this.btnStartaSpelet.Location = new System.Drawing.Point(263, 30);
             this.btnStartaSpelet.Name = "btnStartaSpelet";
             this.btnStartaSpelet.Size = new System.Drawing.Size(83, 23);
             this.btnStartaSpelet.TabIndex = 3;
             this.btnStartaSpelet.Text = "Starta spelet";
             this.btnStartaSpelet.UseVisualStyleBackColor = true;
+            this.btnStartaSpelet.Click += new System.EventHandler(this.btnStartaSpelet_Click);
             // 
             // btnSpelaIgen
             // 
             this.btnSpelaIgen.Enabled = false;
-            this.btnSpelaIgen.Location = new System.Drawing.Point(408, 151);
+            this.btnSpelaIgen.Location = new System.Drawing.Point(254, 51);
             this.btnSpelaIgen.Name = "btnSpelaIgen";
             this.btnSpelaIgen.Size = new System.Drawing.Size(83, 23);
             this.btnSpelaIgen.TabIndex = 4;
@@ -97,7 +102,7 @@
             // lblMinaResultat
             // 
             this.lblMinaResultat.AutoSize = true;
-            this.lblMinaResultat.Location = new System.Drawing.Point(383, 228);
+            this.lblMinaResultat.Location = new System.Drawing.Point(338, 159);
             this.lblMinaResultat.Name = "lblMinaResultat";
             this.lblMinaResultat.Size = new System.Drawing.Size(76, 15);
             this.lblMinaResultat.TabIndex = 5;
@@ -105,17 +110,18 @@
             // 
             // btnGissa
             // 
-            this.btnGissa.Location = new System.Drawing.Point(255, 289);
+            this.btnGissa.Location = new System.Drawing.Point(216, 67);
             this.btnGissa.Name = "btnGissa";
             this.btnGissa.Size = new System.Drawing.Size(75, 23);
             this.btnGissa.TabIndex = 6;
             this.btnGissa.Text = "Gissa";
             this.btnGissa.UseVisualStyleBackColor = true;
+            this.btnGissa.Click += new System.EventHandler(this.btnGissa_Click);
             // 
             // lblDator
             // 
             this.lblDator.AutoSize = true;
-            this.lblDator.Location = new System.Drawing.Point(42, 257);
+            this.lblDator.Location = new System.Drawing.Point(6, 25);
             this.lblDator.Name = "lblDator";
             this.lblDator.Size = new System.Drawing.Size(67, 15);
             this.lblDator.TabIndex = 7;
@@ -124,7 +130,7 @@
             // lblGissa
             // 
             this.lblGissa.AutoSize = true;
-            this.lblGissa.Location = new System.Drawing.Point(39, 292);
+            this.lblGissa.Location = new System.Drawing.Point(6, 71);
             this.lblGissa.Name = "lblGissa";
             this.lblGissa.Size = new System.Drawing.Size(70, 15);
             this.lblGissa.TabIndex = 8;
@@ -132,7 +138,7 @@
             // 
             // tbxGissa
             // 
-            this.tbxGissa.Location = new System.Drawing.Point(125, 289);
+            this.tbxGissa.Location = new System.Drawing.Point(94, 68);
             this.tbxGissa.Name = "tbxGissa";
             this.tbxGissa.Size = new System.Drawing.Size(100, 23);
             this.tbxGissa.TabIndex = 10;
@@ -140,7 +146,7 @@
             // lblDatornsTal
             // 
             this.lblDatornsTal.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
-            this.lblDatornsTal.Location = new System.Drawing.Point(125, 253);
+            this.lblDatornsTal.Location = new System.Drawing.Point(94, 21);
             this.lblDatornsTal.Name = "lblDatornsTal";
             this.lblDatornsTal.Size = new System.Drawing.Size(100, 23);
             this.lblDatornsTal.TabIndex = 11;
@@ -149,31 +155,56 @@
             // lblResultat
             // 
             this.lblResultat.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
-            this.lblResultat.Location = new System.Drawing.Point(68, 353);
+            this.lblResultat.Location = new System.Drawing.Point(16, 110);
             this.lblResultat.Name = "lblResultat";
             this.lblResultat.Size = new System.Drawing.Size(100, 23);
             this.lblResultat.TabIndex = 12;
             this.lblResultat.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
             // 
+            // gbxSpelet
+            // 
+            this.gbxSpelet.Controls.Add(this.btnGissa);
+            this.gbxSpelet.Controls.Add(this.lblResultat);
+            this.gbxSpelet.Controls.Add(this.lblDatornsTal);
+            this.gbxSpelet.Controls.Add(this.lblGissa);
+            this.gbxSpelet.Controls.Add(this.tbxGissa);
+            this.gbxSpelet.Controls.Add(this.lblDator);
+            this.gbxSpelet.Enabled = false;
+            this.gbxSpelet.Location = new System.Drawing.Point(9, 114);
+            this.gbxSpelet.Name = "gbxSpelet";
+            this.gbxSpelet.Size = new System.Drawing.Size(445, 162);
+            this.gbxSpelet.TabIndex = 13;
+            this.gbxSpelet.TabStop = false;
+            this.gbxSpelet.Text = "Spelet";
+            // 
+            // gbxSpeldata
+            // 
+            this.gbxSpeldata.Controls.Add(this.lbxStörstaTalet);
+            this.gbxSpeldata.Controls.Add(this.btnSpelaIgen);
+            this.gbxSpeldata.Controls.Add(this.label1);
+            this.gbxSpeldata.Location = new System.Drawing.Point(9, 8);
+            this.gbxSpeldata.Name = "gbxSpeldata";
+            this.gbxSpeldata.Size = new System.Drawing.Size(445, 100);
+            this.gbxSpeldata.TabIndex = 14;
+            this.gbxSpeldata.TabStop = false;
+            this.gbxSpeldata.Text = "Speldata";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(584, 440);
-            this.Controls.Add(this.lblResultat);
-            this.Controls.Add(this.lblDatornsTal);
-            this.Controls.Add(this.tbxGissa);
-            this.Controls.Add(this.lblGissa);
-            this.Controls.Add(this.lblDator);
-            this.Controls.Add(this.btnGissa);
+            this.ClientSize = new System.Drawing.Size(478, 300);
             this.Controls.Add(this.lblMinaResultat);
-            this.Controls.Add(this.btnSpelaIgen);
             this.Controls.Add(this.btnStartaSpelet);
             this.Controls.Add(this.tbxMinaResultat);
-            this.Controls.Add(this.StörstTal);
-            this.Controls.Add(this.label1);
+            this.Controls.Add(this.gbxSpelet);
+            this.Controls.Add(this.gbxSpeldata);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.gbxSpelet.ResumeLayout(false);
+            this.gbxSpelet.PerformLayout();
+            this.gbxSpeldata.ResumeLayout(false);
+            this.gbxSpeldata.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -182,7 +213,7 @@
         #endregion
 
         private Label label1;
-        private ListBox StörstTal;
+        private ListBox lbxStörstaTalet;
         private TextBox tbxMinaResultat;
         private Button btnStartaSpelet;
         private Button btnSpelaIgen;
@@ -193,5 +224,7 @@
         private TextBox tbxGissa;
         private Label lblDatornsTal;
         private Label lblResultat;
+        private GroupBox gbxSpelet;
+        private GroupBox gbxSpeldata;
     }
 }
--- FormYay/Form1.cs
+++ FormYay/Form1.cs
@@ -2,9 +2,16 @@ namespace FormYay
 {
     public partial class Form1 : Form
     {
+        int datornsTal;
+        int antalGissningar;
         public Form1()
         {
             InitializeComponent();
+
+            lbxStörstaTalet.Items.Add(10);
+            lbxStörstaTalet.Items.Add(20);
+            lbxStörstaTalet.SelectedItem = lbxStörstaTalet.Items[0];
+
         }
 
         private void tbxMinaResultat_TextChanged_1(object sender, EventArgs e)
@@ -13,6 +20,33 @@ namespace FormYay
             tbxMinaResultat.AppendText("Rad 2\n");
             tbxMinaResultat.AppendText("Rad 3\n");
             tbxMinaResultat.AppendText("Rad 4\n");
+        }
+
+        private void btnStartaSpelet_Click(object sender, EventArgs e)
+        {
+            string störstaTal = lbxStörstaTalet.SelectedItem.ToString();
+            int störst = int.Parse(störstaTal);
+            Random slump = new Random();
+            datornsTal = slump.Next(1, störst + 1);
+
+            btnStartaSpelet.Enabled = false;
+            gbxSpeldata.Enabled = false;
+            gbxSpelet.Enabled = true;
+
+            lblDatornsTal.Text = "???";
+            antalGissningar = 0;
+        }
+
+        private void btnGissa_Click(object sender, EventArgs e)
+        {
+            antalGissningar++;
+            string gissa = tbxGissa.Text;
+            int gissatTal = int.Parse(gissa);
+
+            if(gissatTal == datornsTal)
+            {
+
+            }
         }
     }
 }
